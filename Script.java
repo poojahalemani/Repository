4288a63959e5b3d882a7c492edd8b79c5d0b629e
Pooja <pooja@mail.com>
2015-08-21 16:13:30 +1200

    UI5-controls

---
+++ Script.java
@@ -0,0 +1,3 @@
+A lo largo de su producciÃ³n, hay gran cantidad de retratos, corno prueba de su predilecciÃ³n por la figura humana; pero incluso esta difÃ­cil disciplina, Alemany ha sabido incorporarla al estilo y carÃ¡cter poÃ©tico de su obra;
+ interpretando no solo el aspecto fÃ­sico del personaje, sino su personalidad y perfil psicolÃ³gico, mediante simbolismos, consiguiendo asÃ­ un enfoque total y completo del retratado; distinto del concepto convencional del retrato 'acadÃ©mico'.
+Entre los personajes de su labor retratista se cuentan SS.MM. Los Reyes de EspaÃ±a, varios miembros de la Familia Real, rectores de Universidades, acadÃ©micos, Presidentes de Diputaciones, Alcaldes, Decanos de NotarÃ­a, Derecho, Medicinaâ€¦ etc. e innumerables particulares
